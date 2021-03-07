c55fda1c86c8823bea5c249d5e9a6917f856f476
Zenia Gist <zeniagist@gmail.com>
2021-03-07 16:35:03 -0600

    added functionality for selecting movies and viewing content

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import MovieCard from '../movie-card/movie-card';
+
+export class MovieView extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const { movie, onClick } = this.props;
+
+    if (!movie) return null;
+
+    return (
+      <div className="movie-view">
+        <img className="movie-poster" src={movie.ImagePath} />
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <button onClick={() => onClick()}>Back</button>
+      </div>
+
+
+    );
+  }
+}
