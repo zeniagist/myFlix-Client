0888cc93a93867d7cedd0c9a5ca7714294c86685
Zenia Gist <zeniagist@gmail.com>
2021-03-20 16:13:31 -0500

    updated delete favorites button

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import propTypes from 'prop-types';
 import axios from 'axios';
@@ -63,7 +62,7 @@ export class ProfileView extends React.Component {
     const token = localStorage.getItem('token');
 
     axios
-      .delete(`https://myflix-zag.herokuapp.com/users/${username}/FavoriteMovies/${movie}`, {
+      .delete(`https://myflix-zag.herokuapp.com/users/${username}/Movies/${movie}`, {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then(() => {
@@ -72,94 +71,7 @@ export class ProfileView extends React.Component {
       })
       .catch(function (error) {
         console.log(error);
-      });
-  }
-
-  handleUpdate(e, newUsername, newPassword, newEmail, newBirthday) {
-    this.setState({
-      validated: null,
-    });
-
-    const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
-      this.setState({
-        validated: true,
-      });
-      return;
-    }
-    e.preventDefault();
-
-    const token = localStorage.getItem('token');
-    const username = localStorage.getItem('user');
-
-    axios({
-      method: 'put',
-      url: `https://myflix-zag.herokuapp.com/users/${username}`,
-      headers: { Authorization: `Bearer ${token}` },
-      data: {
-        Username: newUsername ? newUsername : this.state.Username,
-        Password: newPassword ? newPassword : this.state.Password,
-        Email: newEmail ? newEmail : this.state.Email,
-        Birthday: newBirthday ? newBirthday : this.state.Birthday,
-      },
-    })
-      .then((response) => {
-        this.setState({
-          Username: response.data.Username,
-          Password: response.data.Password,
-          Email: response.data.Email,
-          Birthday: response.data.Birthday,
-        });
-        alert('Changes have been saved!');
-        localStorage.setItem('user', this.state.Username);
-        // this.props.history.push(`/users/${username}`);
-        window.location.pathname = `/users/${username}`
-        // console.log(response.data);
-
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  setUsername(input) {
-    this.Username = input;
-  }
-
-  setPassword(input) {
-    this.Password = input;
-  }
-
-  setEmail(input) {
-    this.Email = input;
-  }
-
-  setBirthday(input) {
-    this.Birthday = input;
-  }
-
-  handleDeregister(e) {
-    e.preventDefault();
-
-    const token = localStorage.getItem('token');
-    const username = localStorage.getItem('user');
-
-    axios
-      .delete(`https://myflix-zag.herokuapp.com/users/${username}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then(() => {
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
-        alert('Your account has been deleted');
-        // this.props.history.push(`/`);
-        window.location.pathname = `/`
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+      }).then(() => window.location.reload());
   }
 
   render() {
@@ -169,9 +81,11 @@ export class ProfileView extends React.Component {
 
     return (
       <Container className='profile-view'>
-        <Tabs defaultActiveKey='profile' transition={false} className='profile-tabs' style={{ textDecoration: 'none' }}>
+        <Tabs defaultActiveKey='profile' transition={false} className='profile-tabs'>
+
+
           <Tab className='tab-item' eventKey='profile' title='Profile'>
-            <Card className='profile-card'>
+            <Card className='profile-card' border='info'>
               <Card.Title className='profile-title'>{username}'s Favorite Movies</Card.Title>
               {FavoriteMovies.length === 0 && <div className='card-content'>You don't have any favorite movies yet!</div>}
 
@@ -181,12 +95,11 @@ export class ProfileView extends React.Component {
                     if (movie._id === FavoriteMovies.find((favMovie) => favMovie === movie._id)) {
                       return (
                         <div key={movie._id}>
-                          <Card className='favorites-card card-content' style={{ width: '16rem', flex: 1 }}>
+                          <Card className='favorites-item card-content' style={{ width: '16rem', flex: 1 }}>
                             <Link to={`/movies/${movie._id}`}>
-                              <Card.Img className='favorites-movie' src={movie.ImagePath} />
+                              <Card.Img className='movie-card' variant="top" src={movie.ImagePath} />
                             </Link>
                             <Card.Title className='movie-card-title'>{movie.Title}</Card.Title>
-                            <Card.Subtitle className='text-muted fav-subtitle'>{movie.Year}</Card.Subtitle>
                             <Card.Body className='movie-card-body'>
                               <Button size='sm' className='profile-button remove-favorite' variant='danger' onClick={(e) => this.handleRemoveFavorite(e, movie._id)}>
                                 Remove
@@ -201,59 +114,6 @@ export class ProfileView extends React.Component {
             </Card>
           </Tab>
 
-
-          <Tab className='tab-item' eventKey='update' title='Update'>
-            <Card className='update-card' border='info'>
-              <Card.Title className='profile-title'>Update Profile</Card.Title>
-              <Card.Subtitle className='card-subtitle-update'>If you are not updating a certain field (ex; email), then leave that field empty.
-                  <span className='password-instructions'>*You must enter in either a new or existing password to verify the change!</span>
-              </Card.Subtitle>
-              <Card.Body>
-                <Form noValidate validated={validated} className='update-form' onSubmit={(e) => this.handleUpdate(e, this.Username, this.Password, this.Email, this.Birthday)}>
-                  <Form.Group controlId='formBasicUsername'>
-                    <Form.Label className='form-label'>Username</Form.Label>
-                    <Form.Control type='text' placeholder='Change Username' onChange={(e) => this.setUsername(e.target.value)} pattern='[a-zA-Z0-9]{5,}' />
-                    <Form.Control.Feedback type='invalid'>Please enter a valid username with at least 5 alphanumeric characters.</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId='formBasicPassword'>
-                    <Form.Label className='form-label'>
-                      Password <span className='required'>*</span>
-                    </Form.Label>
-                    <Form.Control type='password' placeholder='Current or New Password' onChange={(e) => this.setPassword(e.target.value)} pattern='.{5,}' />
-                    <Form.Control.Feedback type='invalid'>Please enter a valid password with at least 5 characters.</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId='formBasicEmail'>
-                    <Form.Label className='form-label'>Email</Form.Label>
-                    <Form.Control type='email' placeholder='Change Email' onChange={(e) => this.setEmail(e.target.value)} />
-                    <Form.Control.Feedback type='invalid'>Please enter a valid email address.</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId='formBasicBirthday'>
-                    <Form.Label className='form-label'>Birthday</Form.Label>
-                    <Form.Control type='date' placeholder='Change Birthday' onChange={(e) => this.setBirthday(e.target.value)} />
-                    <Form.Control.Feedback type='invalid'>Please enter a valid birthday.</Form.Control.Feedback>
-                  </Form.Group>
-                  <Button className='update-profile-button' type='submit' variant='info'>
-                    Update
-                  </Button>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Tab>
-
-
-          <Tab className='tab-item' eventKey='delete' title='Delete Profile'>
-            <Card className='update-card' border='danger'>
-              <Card.Title className='profile-title'>Delete Your Profile</Card.Title>
-              <Card.Subtitle className='text-muted'>If you delete your account, it cannot be recovered.</Card.Subtitle>
-              <Card.Body>
-                <Button className='button' variant='danger' onClick={(e) => this.handleDeregister(e)}>
-                  Click Here If You're Sure!
-				        </Button>
-              </Card.Body>
-            </Card>
-          </Tab>
-
-
         </Tabs>
       </Container>
     );
@@ -264,8 +124,7 @@ ProfileView.propTypes = {
   user: propTypes.shape({
     FavoriteMovies: propTypes.arrayOf(
       propTypes.shape({
-        _id: propTypes.string.isRequired,
-        Title: propTypes.string.isRequired,
+        _id: propTypes.string.isRequired
       })
     ),
     Username: propTypes.string.isRequired,
