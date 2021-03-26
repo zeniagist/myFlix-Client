5ba84b6cb31d61825c94d9b2461094beb83350c3
Zenia Gist <zeniagist@gmail.com>
2021-03-26 05:41:58 -0500

    added store for actions and reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// React Redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 import { MainView } from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
 
 // Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
+// Create React Redux Store
+// const store = createStore(moviesApp);
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
   }
 }
 
