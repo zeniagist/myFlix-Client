eab2783f3c2abc51dab94b9785bc572d386c9e98
Zenia Gist <zeniagist@gmail.com>
2021-03-30 14:14:48 -0500

    updated window reloads

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -68,7 +68,8 @@ export class ProfileView extends React.Component {
       })
       .then(() => {
         alert('Movie was removed from your Favorites List.');
-        this.componentDidMount();
+        // this.componentDidMount();
+        window.location.reload()
       })
       .catch(function (error) {
         console.log(error);
@@ -115,7 +116,8 @@ export class ProfileView extends React.Component {
         alert('Changes have been saved!');
         localStorage.setItem('user', this.state.Username);
         // this.props.history.push(`/users/${username}`);
-        window.location.pathname = `/users/${username}`
+        window.location.reload()
+        // window.location.pathname = `/users/${username}`
         // console.log(response.data);
 
       })
@@ -155,7 +157,8 @@ export class ProfileView extends React.Component {
         localStorage.removeItem('token');
         alert('Your account has been deleted');
         // this.props.history.push(`/`);
-        window.location.pathname = `/`
+        window.location.reload()
+        // window.location.pathname = `/`
       })
       .catch((e) => {
         console.log(e);
